2cb5ae8f602145b3d05061c60adcb77da5e16b11
Jimmy WU <cmwuaa@connect.ust.hk>
2023-10-05 01:38:34 +0800

    Included Quick Demo Function

---
+++ Quick Demo/p5js.js
@@ -0,0 +1,142 @@
+/* export SVG
+DDF 2018
+need to have p5.svg.js in project and in index.html
+see -https://github.com/zenozeng/p5.js-svg
+this will save an SVG file in your download folder
+*/
+
+var cnv;
+var Objects = [];
+var NB = 40;
+var seed_list = [164, 505, 5906, 8369]
+
+var wid = 609;
+var hei = 368;
+
+var NB_FRAMES = 60; // *************smaller NB_FRAMES faster reaction, while higher will make slower*************
+
+var frame_count = 60;
+
+function mapRange(value, inputMin, inputMax, outputMin, outputMax) {
+  const inputRange = inputMax - inputMin;
+  const outputRange = outputMax - outputMin;
+  const scaledValue = (value - inputMin) / inputRange;
+  const mappedValue = scaledValue * outputRange + outputMin;
+  return Math.round(mappedValue);
+}
+
+// function graph_update (){ // *************this function will update the NB_FRAMES every draw loop by mqtt data*************
+//   NB_FRAMES = 60 / water_flow;
+//   NB_FRAMES = NB_FRAMES.toFixed(0)
+//   console.log('NB_FRAMES:', NB_FRAMES);
+//   return NB_FRAMES
+// }
+
+function graph_update (){
+  var seed_idx = mapRange(water_consum, 0, 200, 0, 3);
+  var last_seed = curSeed;
+  curSeed = seed_list[seed_idx];
+  if (last_seed == curSeed){
+    noiseSeed(curSeed);
+    console.log("curSeed updated to: " + curSeed);
+  }
+}
+
+function activation(t) {
+  return ((1 - cos(2 * PI * t)) / 2) ** 1;
+}
+
+
+function setup() {
+
+  curSeed = 164;
+  noiseSeed(curSeed);
+  randomSeed(1);
+
+  createCanvas(wid, hei, SVG);
+  strokeWeight(1);   // do 0.1 for laser
+  stroke(255, 0, 0);      // red is good for laser
+  noFill();
+  //cnv.parent("canvas");
+
+  background(0);
+
+  for (var i = 0; i < NB; i++) {
+    Objects[i] = new object(i);
+
+  }
+  // better not to have a fill for laser
+}
+
+function object(id) {
+
+  this.id = id;
+
+  this.draw = function (NB_FRAMES) {// *************force it use the latest NB_FRAMES to count the rate*************
+    var t = ((frame_count) % NB_FRAMES) / NB_FRAMES;
+
+    var x0 = lerp(0, wid, this.id / NB);
+
+    theta = PI / 2;
+
+    var xx = x0;
+    var yy = 0;
+
+    var Nt = 75; // this can change the drawing style // original (Nt = 75)
+
+    var step = hei / Nt;
+
+    var turn = lerp(0, 0.4, activation((this.id / NB + 0 * t) % 1));
+
+    stroke(255);
+    strokeWeight(1);
+    noFill();
+    beginShape();
+
+    vertex(xx, yy);
+
+
+    for (var i = 0; i <= Nt; i++) {
+      theta += turn * sin(100 * noise(1000) + 2 * PI * (15 * noise(0.2 * this.id / NB, 0.02 * i) + t));
+      xx += step * cos(theta);
+      yy += step * sin(theta);
+
+      var xx2 = lerp(xx, x0, (i / Nt) * (i / Nt) * (i / Nt));
+      var yy2 = lerp(yy, lerp(0, hei - 0, i / Nt), max((i / Nt), 1 - sqrt(i / Nt)));
+
+      vertex(xx2, yy2);
+
+
+    }
+    endShape();
+
+  }
+}
+
+// function mousePressed(){
+//     curSeed = floor(random()*10000);
+//     noiseSeed(curSeed);
+//     console.log(curSeed);
+// }
+
+function draw() {
+  background(192, 225, 248);
+
+  graph_update();
+  for (var i = 0; i < NB; i++) { 
+    Objects[i].draw(NB_FRAMES);
+  }
+
+  noStroke();
+  fill(255);
+  text("seed : " + curSeed, 10, 10);
+
+  frame_count++;
+
+  //////////////////////////////////////EXPORT
+  if (keyCode === LEFT_ARROW) {
+    save("mySVG.svg");
+    print("saved svg");
+    noLoop();
+  }
+}
---
+++ Quick Demo/react.js
@@ -0,0 +1,51 @@
+var water_consum = 0; // in ml
+
+// Function to get the current date in the user's local timezone with format: 23th September 2023
+function get_date() {
+  var now = new Date();
+  var options = { day: 'numeric', month: 'long', year: 'numeric' };
+  return now.toLocaleDateString('en-GB', options);
+}
+
+// Function to get the current time in the user's local timezone with format: 06 : 23 PM with pad zero
+function get_time() {
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var ampm = hours >= 12 ? 'PM' : 'AM';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // Handle midnight (12:00 AM)
+
+  return padZero(hours) + " : " + padZero(minutes) + " " + ampm;
+}
+
+// Helper function to pad zero for hours and minutes
+function padZero(num) {
+  return (num < 10 ? "0" : "") + num;
+}
+
+function flow_update(data) {
+  var datatype = +data;
+  if (!isNaN(datatype)){
+    if (data > 200){
+      data = 200;
+      console.log("data flow received too large")
+    }
+    water_consum = data*10;
+    console.log('total water consumtion: ' + water_consum);
+  }
+  else {
+    console.log("Non number data type received: " + typeof datatype);
+  }
+  
+  
+}
+
+// get the current date in Hong Kong Time Zone
+function get_bottle() {
+  var num_bottle;
+  num_bottle = water_consum * 2; // L x 1000ml / 500ml per bottle
+  num_bottle = num_bottle.toFixed(1);
+  console.log('update num of bottle:' + num_bottle);
+  return num_bottle;
+}
